5cf5dc8de0a22f1e42db644368c0cf36116b3b16
d06hara <d06.harada.prg@gmail.com>
2020-11-17 20:47:32 +0900

    スペアストライク無視、最後まで出力させるまで

--- bowling.rb
+++ bowling.rb
@@ -15,26 +15,50 @@ def playball
   puts 'Play Ball !!'
   puts '============'
 
-  player = "A"
+  player = "Player A"
 
   # 点数の付け方
   # frame x の一投目をscore x1、二投目をscore x2 とする
   # frame x の合計点数をtotal_score x とする
-  score11 = rand(10)
-  score12 = rand(10)
-  total_score1 = score11 + score12
-  puts score11
-  puts score12
-  puts total_score1
+  # score11 = rand(10)
+  # score12 = rand(10)
+  # total_score1 = score11 + score12
+  # puts score11
+  # puts score12
+  # puts total_score1
 
-  # for i in (1..10) do
-  #   puts i
-  # end
+  # 合計スコア
+  total_score = 0
+  # 各フレームのスコアを入れる配列を用意
+  frame_scores = []
+
+  # スコアボード
+  score_board = "  #{player}  |"
+
+  for i in (0..9) do
+    # １投目と２投目
+    score1 = rand(10)
+    score2 = rand(10)
+    # 合計を計算し、配列に代入
+    frame_score = score1 + score2
+    frame_scores[i] = frame_score
+    # puts frame_scores[i]
+    puts "スコアは#{frame_scores[i]}"
+
+    # 合計スコアに加算していく
+    total_score += frame_scores[i]
+
+    # 出力用スコアボードに文字列として連結していく
+    score_board += "   #{frame_scores[i]}   |"
+  end
+
+  puts "合計スコアは#{total_score}"
+  score_board += "　　#{total_score}　　|"
+
 
   #結果出力
-  puts ''
-  puts 'フレーム|   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |     10    |　　合計　　|'
-  puts "　　#{player}　　|   #{total_score1}   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |     10    |　　　　　　|"
+  puts '  フレーム  |   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |     10    |　　合計　　|'
+  puts score_board
 end
 
 playball
---
+++ test.rb
@@ -0,0 +1,6 @@
+score_board = 'スコア　　|'
+for i in (0..9)
+  score_board += "  #{i}  |"
+end
+
+puts score_board
