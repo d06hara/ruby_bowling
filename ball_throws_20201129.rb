c1e76cd7aaae42c50812cf1a7609ccba07442d52
d06hara <d06.harada.prg@gmail.com>
2020-11-29 20:43:21 +0900

    投球メソッド break使用

--- ball_throws_20201129.rb
+++ ball_throws_20201129.rb
@@ -1,15 +1,19 @@
-# scoreは1つの数字
-score = 0
-# scoresは1フレームあたりの特典
-scores = []
+#===================
+# 変数,定数
+#===================
 # total_scoresはscoresの集まり
 total_scores = []
 MAX_PINS = 10
 STRIKE = 10
+SPARE = 10
 MAX_FRAME = 10
+SPARE_ELEMENTS = 2
+#---------------------
 
-# 球を投げて返すだけ
-# 残りのピンを別のメソッドから受け取ってそれに対し球を投げて返すだけ
+# ==================
+# メソッド
+# ==================
+# 残りのピンを受け取ってそれに対し球を投げて返すだけ
 def ball_throw(remaining_pins)
   rand(0..remaining_pins)
 end
@@ -28,54 +32,45 @@ def calc_pins(score, frame)
     MAX_PINS - score
   end
 end
+#------------------------
 
-# 10フレーム投げる
-(1..10).each do |frame|
+
+
+(1..MAX_FRAME).each do |frame|
   # scoresを初期化
-  score = 0
-  scores = []
+  score = 0 #１投のスコア
+  scores = [] # １フレームのスコア
   if frame < MAX_FRAME
     (1..2).each do |throws|
-      # ストライクかどうかを判定
-      if scores.sum != STRIKE
-        # 残りのピンを計算
-        remaining_pins = calc_pins(score, frame)
-        # 投げる
-        score = ball_throw(remaining_pins)
-        # スコアを記入
-        fill_in_score(scores, score)
+      # 残りのピンを計算
+      remaining_pins = calc_pins(score, frame)
+      # 投げる
+      score = ball_throw(remaining_pins)
+      # スコアを記入
+      fill_in_score(scores, score)
+      # ストライクの場合はループを抜ける
+      if scores.sum == STRIKE
+        break
       end
     end
   else
     (1..3).each do |throws|
-      if throws < 3
-          # 残りのピンを計算
-          puts "score:#{score}"
-          puts "frame:#{frame}"
-          remaining_pins = calc_pins(score, frame)
-          puts "remaining_pins:#{remaining_pins}"
-          # 投げる
-          score = ball_throw(remaining_pins)
-          puts "score:#{score}"
-          # 記入
-          fill_in_score(scores, score)
-          print "#{scores} \n"
-      else
-        if scores.sum >= STRIKE
-          # 残りのピンを計算
-          puts "score:#{score}"
-          puts "frame:#{frame}"
-          remaining_pins = calc_pins(score, frame)
-          puts "remaining_pins:#{remaining_pins}"
-          # 投げる
-          score = ball_throw(remaining_pins)
-          puts "score:#{score}"
-          # 記入
-          fill_in_score(scores, score)
-          print "#{scores} \n"
-        end
-      end
+      # 残りのピンを計算
+      puts "score:#{score}"
+      puts "frame:#{frame}"
+      remaining_pins = calc_pins(score, frame)
+      puts "remaining_pins:#{remaining_pins}"
+      # 投げる
+      score = ball_throw(remaining_pins)
+      puts "score:#{score}"
+      # 記入
+      fill_in_score(scores, score)
+      print "#{scores} \n"
 
+      # ２投投げ終わってかつ、スペアでない場合はbreak
+      if scores.length == SPARE_ELEMENTS && scores.sum != SPARE
+        break
+      end
     end
   end
 
