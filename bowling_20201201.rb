f257cf1b956c7689f3e8b092d499645a2d795a0d
d06hara <d06.harada.prg@gmail.com>
2020-12-03 20:42:56 +0900

    reviewファイルの作成

--- bowling_20201201.rb
+++ bowling_20201201.rb
@@ -6,8 +6,15 @@ total_scores = []
 MAX_PINS = 10
 STRIKE = 10
 SPARE = 10
-MAX_FRAME = 10
+MAX_FRAME = 4
+STRIKE_ELEMENT = 1
 SPARE_ELEMENTS = 2
+
+
+# 加点用変数
+add_array = []
+# 練習用配列
+pra_array = []
 #---------------------
 
 # ==================
@@ -31,6 +38,17 @@ def calc_pins(scores, score, frame)
     MAX_PINS - score
   end
 end
+
+def judge_scores(scores)
+  if scores.length == STRIKE_ELEMENT && scores.sum == STRIKE
+    'STRIKE'
+  elsif scores.length == SPARE_ELEMENT && scores.sum == SPARE
+    'SPARE'
+  else
+    'NOT_STRIKE_OR_SPARE'
+  end
+end
+
 #------------------------
 
 (1..MAX_FRAME).each do |frame|
@@ -40,12 +58,61 @@ end
   (1..3).each do |throws|
     score = ball_throw(calc_pins(scores, score, frame))
     fill_in_score(scores, score)
+
+
+    # # 加点を入れるならここ
+    # # この前までの出力
+    # # score = x, scores = [x] or [x, y]
+    
+    # puts ''
+    # print "pra_array: #{pra_array}"
+    # puts ''
+
+    # # 前のフレームの配列を取得
+    # if frame > 1
+    #   # 前のフレームがストライクの時
+    #   if add_array.length == STRIKE_ELEMENT && add_array.sum == STRIKE
+
+    #   elsif
+        
+    #   end
+    # end
+
+
+    # add_array.push(score)
+    # puts ''
+    # print "add_array: #{add_array}"
+    # puts ''
+    # puts "#{frame}フレーム目の状態"
+
+
+
+
+
+
+
+
+
     # 最終フレーム以外でかつスコアの合計が10であればbreak、2投目でかつスコアの合計がスペア以下の場合break
     if (frame != MAX_FRAME && (scores.sum == (STRIKE || SPARE))) || (throws == 2 && scores.sum < SPARE)
       break
     end
+
+
   end
+
+
+  before_frame = frame - 1
+  print total_scores
+  print total_scores[before_frame]
+  flatten_total_scores = total_scores.flatten
+  puts ''
+  print flatten_total_scores
+  puts ''
+
+
   fill_in_score(total_scores, scores)
+  # fill_in_score(pra_array, scores)
 end
 
 print total_scores
