23a574dd7a84312b829720185808815beb530daa
d06hara <d06.harada.prg@gmail.com>
2020-12-07 21:44:22 +0900

    共通動作のメソッド化、フレームによる分岐削除

--- ball_throws_20201206.rb
+++ ball_throws_20201206.rb
@@ -7,7 +7,7 @@ scores = []
 # MAX_PINS = 10
 # STRIKE = 10
 # SPARE = 10
-MAX_FRAME = 4
+FINISH_FRAME = 4
 # SPARE_ELEMENTS = 2
 #---------------------
 
@@ -18,18 +18,18 @@ class BallThrow
   MAX_PINS = 10
   STRIKE = 10
   SPARE = 10
-  MAX_FRAME = 10
+  MAX_FRAME = 4
   STRIKE_ELEMENT = 1
   SPARE_ELEMENTS = 2
 
   # クラス変数
   @@total_scores = []
+  @@total_frames = 0
 
   # 確認用のアクセサ
   attr_accessor :score
   attr_accessor :scores
   attr_accessor :remaining_pins
-
 
   # 初期化
   def initialize(score = 0, scores = [], remaining_pins = 10)
@@ -37,6 +37,8 @@ class BallThrow
     @scores = scores
     @remaining_pins = remaining_pins
     @@total_scores
+    # total_framesはインスタンス生成するたびに1をたす
+    @@total_frames += 1
   end
 
   # 残りのピンを受け取ってそれに対し球を投げて返すだけ
@@ -47,6 +49,7 @@ class BallThrow
   # 投げられた球を受け取って記入
   def fill_in_score
     @scores.push(@score)
+    puts "#{@scores}"
   end
   # フレームスコアを合計スコアに入れる
   def scores_to_total_scores
@@ -54,16 +57,14 @@ class BallThrow
   end
 
   # 集計結果を受け取って、残りのピンを計算する
-  def calc_pins(frame)
-    frame == MAX_FRAME ? @remaining_pins : @remaining_pins = MAX_PINS - @score
-    # # if frame == MAX_FRAME && (@scores.sum == STRIKE || @scores.sum == SPARE)
-    # if frame == MAX_FRAME
+  def calc_pins
+    (last_frame?) ? @remaining_pins : @remaining_pins = MAX_PINS - @score
+    # frame == MAX_FRAME ? @remaining_pins : @remaining_pins = MAX_PINS - @score
+  end
 
-    #   @remaining_pins
-    # else
-    #   # return MAX_PINS - @score #return が必要だったのはなぜ？
-    #   @remaining_pins = MAX_PINS - @score #return が必要だったのはなぜ？
-    # end
+  # スコアの合計を計算する
+  def sum_scores
+    @scores.sum
   end
 
   # ストライクを判定する
@@ -75,54 +76,38 @@ class BallThrow
   def spare?
     @scores.sum == SPARE && @scores.length == SPARE_ELEMENTS ? true : false
   end
+  
+  # フレームの終了を判定する (最終フレーム以外はstrikeになった時、最終フレームは2投の合計が10以下の時)
+  def finish_frame?
+    (!(last_frame?) && strike?) || (last_frame? && (@scores.length == SPARE_ELEMENTS && sum_scores < SPARE)) ? true : false
+  end
+
+  # 最終フレームの判定
+  def last_frame?
+    @@total_frames == MAX_FRAME ? true : false
+  end
 
 end
 
 #------------------------
 
-# totalスコアのインスタンスを最初に生成
-# collect_score = CollectScore.new
 
-(1..MAX_FRAME).each do |frame|
+(1..FINISH_FRAME).each do |frame|
 
   # インスタンス化
   ball_throw = BallThrow.new
-
-
-  if (frame == MAX_FRAME)
-    (1..3).each do |throws|
-      puts '残りの球を計算する'
-      ball_throw.calc_pins(frame)
-  
-      puts '球を投げる'
-      ball_throw.one_throw
-      puts 'スコアを記入する'
-      ball_throw.fill_in_score
-
-    end
-  else
-    (1..2).each do |throws|
-      puts '残りの球を計算する'
-      ball_throw.calc_pins(frame)
-  
-      puts '球を投げる'
-      ball_throw.one_throw
-      puts 'スコアを記入する'
-      ball_throw.fill_in_score
-      
-      if(ball_throw.strike?)
-        break
-      end
-
-      # 最終フレーム以外でかつスコアの合計が10であればbreak、2投目でかつスコアの合計がスペア以下の場合break
-      # if (frame != MAX_FRAME && (scores.sum == (STRIKE || SPARE))) || (throws == 2 && scores.sum < SPARE)
-      #   break
-      # end
+  # フレーム数によって最大投球数を変える
+  max_throw = (ball_throw.last_frame?) ? 3 : 2
+  (1..max_throw).each do |throws|
+    ball_throw.calc_pins #残りの球を計算する
+    ball_throw.one_throw #球を投げる
+    ball_throw.fill_in_score #スコアを記入する
+    if (ball_throw.finish_frame?)
+      puts '終了します'
+      break
     end
   end
 
+  puts ''
   print ball_throw.scores_to_total_scores
 end
-
-
-# print total_scores
